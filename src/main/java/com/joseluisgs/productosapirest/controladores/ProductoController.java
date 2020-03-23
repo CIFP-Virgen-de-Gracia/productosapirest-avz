c92f936549f7b2ffb067b667fd0bc204d5f08b10
joseluisgs <joseluisgonsan@gmail.com>
2020-03-23 11:52:08 +0100

    ManyToOne

--- src/main/java/com/joseluisgs/productosapirest/controladores/ProductoController.java
+++ src/main/java/com/joseluisgs/productosapirest/controladores/ProductoController.java
@@ -168,9 +168,9 @@ public class ProductoController {
         } else {
 
             // Con ModelMapper
-            Page<ProductoDTO> dtoList = result.map(productoDTOConverter::convertToDto);
+            //Page<ProductoDTO> dtoList = result.map(productoDTOConverter::convertToDto);
             // Con Lombok @Builder de DTO
-            //Page<ProductoDTO> dtoList = result.map(productoDTOConverter::convertProdutoToProductoDto);
+            Page<ProductoDTO> dtoList = result.map(productoDTOConverter::convertProdutoToProductoDto);
             UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(request.getRequestURL().toString());
 
             return ResponseEntity.ok().header("link", paginationLinksUtils.createLinkHeader(dtoList, uriBuilder))
